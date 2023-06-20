f58e198c37e2cc0cc8af5f60b6ae8259338a7a3f
ka7de <133555923+ka7de@users.noreply.github.com>
2023-06-20 10:55:29 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,92 @@
+// スムーススクロール
+$(function () {
+    $('a[href^="#"]').click(function(){
+        var speed = 500;
+        var type = 'swing';
+        var href =$(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({'scrollTop':position},speed,type);
+        return false;
+    });
+});
+
+// フェードイン
+$(function(){
+    $('.fadeOut').on('inview',function(){
+        $(this).addClass('fadeIn');
+    });
+});
+
+// 拡大して表示
+$(function (){
+    $('.catch-flex-odd').on('inview',function(){
+        $(this).addClass('show1');
+    });
+    $('.catch-flex-even').on('inview',function(){
+        $(this).addClass('show1');
+    });
+    $('.feature-contents-study-item').on('inview',function(){
+        $(this).addClass('show1');
+    })
+    $('.support-contents-item').on('inview',function(){
+        $(this).addClass('show1');
+    });
+    $('.price-back').on('inview',function(){
+        $(this).addClass('show1');
+    });
+});
+
+// 横からスライド
+$(function (){
+    $('.curriculum-desc-odd').on('inview',function(){
+        $(this).addClass('show2');
+    });
+    $('.curriculum-desc-even').on('inview',function(){
+        $(this).addClass('show2');
+    });
+});
+
+// アコーディオンメニュー
+$(function (){
+    $('.accordion-title').on('click',function(){
+        $('.accordion-desc').slideUp(500);
+
+        var findElm = $(this).next('.accordion-desc');
+        if($(this).hasClass('close')){
+            $(this).removeClass('close');
+        } else {
+            $('.close').removeClass('close');
+            $(this).addClass('close');
+            $(findElm).slideDown(500);
+        }
+    });
+
+    $('.more-btn').on('click',function(){
+        var textArea = $(this).next('.additional-text-area');
+        $(textArea).slideToggle(500);
+        if($(this).hasClass('up')){
+            $(this).removeClass('up');
+        } else {
+            $(this).addClass('up');
+        }
+    });
+});
+
+// ハンバーガーメニュー
+$(function(){
+    $('#hamburger').click(function(){
+        $(this).toggleClass('active');
+        $('#slide-menu').toggleClass('active');
+    });
+
+    $('.hamburger-nav-item a').click(function(){
+        $('#hamburger').removeClass('active');
+        $('#slide-menu').removeClass('active');
+    });
+
+    $('#hamburger-btn').click(function(){
+        $('#hamburger').removeClass('active');
+        $('.slide-menu').removeClass('active');
+    });
+});
